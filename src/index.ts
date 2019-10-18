99eb4e2b9d71d6a63379272bfc5a80f3207a8ae8
Rohit Gohri <gohri.rohit@gmail.com>
2019-10-18 23:41:37 +0530

    feat(plugin): Add basic logic for todo matching

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,81 @@
 // Provides dev-time type structures for  `danger` - doesn't affect runtime.
-import {DangerDSLType} from "../node_modules/danger/distribution/dsl/DangerDSL"
+import { DangerDSLType } from "../node_modules/danger/distribution/dsl/DangerDSL"
 declare var danger: DangerDSLType
 export declare function message(message: string): void
 export declare function warn(message: string): void
 export declare function fail(message: string): void
 export declare function markdown(message: string): void
 
+function getCreatedOrModifiedFiles() {
+  return [...danger.git.created_files, ...danger.git.modified_files]
+}
+
 /**
  * A danger-js plugin to list all todos/fixmes/etc added/changed in a PR
  */
-export default function todos() {
-  // Replace this with the code from your Dangerfile
-  const title = danger.github.pr.title
-  message(`PR Title: ${title}`)
+export default async function todos({
+  repoUrl = "",
+  ignore = [],
+  keywords = ["TODO", "FIXME"],
+}: {
+  repoUrl?: string
+  ignore?: Array<string | RegExp>
+  keywords?: string[]
+} = {}) {
+  const keywordMatches: { [key: string]: string[] } = {}
+
+  keywords.forEach(keyword => {
+    keywordMatches[keyword] = []
+  })
+
+  await Promise.all(
+    getCreatedOrModifiedFiles().map(async filepath => {
+      let ignoreFile = false
+      ignore.forEach(ignorePath => {
+        if (typeof ignorePath === "string" && filepath.includes(ignorePath)) {
+          ignoreFile = true
+        } else if (ignorePath instanceof RegExp && ignorePath.test(filepath)) {
+          ignoreFile = true
+        }
+      })
+
+      if (ignoreFile) {
+        return
+      }
+
+      const diff = await danger.git.diffForFile(filepath)
+
+      if (!diff) {
+        return
+      }
+
+      keywords.forEach(keyword => {
+        const regex = new RegExp(`(?:\/\/|#|<!--|;|\/\*|^|\/\*\*\s*\**)\s*${keyword.trim()}(?::\s*|\s+)(.+)`, "gi")
+        const matches = diff.added.match(regex)
+        const srcLink = repoUrl
+          ? `[\`${filepath}\`](${repoUrl}/blob/${danger.git.commits[0].sha}/${filepath})`
+          : `\`${filepath}\``
+
+        if (matches) {
+          matches.forEach(match => {
+            keywordMatches[keyword].push(`\`\`${match}\`\` : ${srcLink}`)
+          })
+        }
+      })
+    })
+  )
+
+  const output: string[] = []
+  Object.values(keywordMatches).forEach(matches => {
+    if (matches.length) {
+      output.push(...matches)
+    }
+  })
+
+  if (!output.length) {
+    return
+  }
+
+  markdown(`### ${keywords.join("s/")}s:
+- ${output.join("\n- ")}`)
 }
