4268d214a323660596a44b06e61ed3ec0ee33c60
Rohit Gohri <gohri.rohit@gmail.com>
2020-03-06 00:42:52 +0530

    ci: add test fail danger

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,10 +1,12 @@
-import { schedule, warn } from 'danger'
+import { schedule, fail } from 'danger'
 import todos from './src'
 
 async function main() {
   await todos({
     keywords: ['TODO', 'FIXME', 'WTF'],
   })
+
+  fail('test')
 }
 
 schedule(main())
